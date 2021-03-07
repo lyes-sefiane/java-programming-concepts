fc2249f50392cc5cb09adf17c0cae70302936c7e
lsefiane <lyes.sefiane@gmail.com>
2021-03-06 20:52:50 -0500

    ~ Init

---
+++ beautiful-java-functional-programming-sample/src/main/java/com/lsefiane/beautiful/java/functional/programming/demo/BeautifulJavaFunctionalProgrammingSampleApplication.java
@@ -0,0 +1,13 @@
+package com.lsefiane.beautiful.java.functional.programming.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulJavaFunctionalProgrammingSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulJavaFunctionalProgrammingSampleApplication.class, args);
+	}
+
+}
---
+++ beautiful-java-functional-programming-sample/src/test/java/com/lsefiane/beautiful/java/functional/programming/demo/BeautifulJavaFunctionalProgrammingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsefiane.beautiful.java.functional.programming.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulJavaFunctionalProgrammingSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
