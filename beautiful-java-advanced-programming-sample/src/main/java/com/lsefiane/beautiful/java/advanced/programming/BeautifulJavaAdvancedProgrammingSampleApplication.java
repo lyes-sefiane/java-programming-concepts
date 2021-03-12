f574f369240648d4814db0e4824ef8fe393de8e9
lsefiane <lyes.sefiane@gmail.com>
2021-03-12 13:50:45 -0500

    ~ Init

---
+++ beautiful-java-advanced-programming-sample/src/main/java/com/lsefiane/beautiful/java/advanced/programming/BeautifulJavaAdvancedProgrammingSampleApplication.java
@@ -0,0 +1,13 @@
+package com.lsefiane.beautiful.java.advanced.programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulJavaAdvancedProgrammingSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulJavaAdvancedProgrammingSampleApplication.class, args);
+	}
+
+}
---
+++ beautiful-java-advanced-programming-sample/src/test/java/com/lsefiane/beautiful/java/advanced/programming/BeautifulJavaAdvancedProgrammingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsefiane.beautiful.java.advanced.programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulJavaAdvancedProgrammingSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
